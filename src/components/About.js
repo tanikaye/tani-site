18e07ef87e06e47427e5915adfe2af16b7bf3cac
Tani Kaye <1tanikaye@gmail.com>
2022-07-20 00:00:34 -0400

    test commit for post deployment

--- src/components/About.js
+++ src/components/About.js
@@ -16,7 +16,7 @@ function About() {
             <p>Hi. I'm Tani, nice to meet you. Please take a look around.</p>
           </div>
           <div>
-            <p>I am passionate about building excellent software that improves
+            <p>I'm passionate about building excellent software that improves
             the lives of those around me. I specialize in creating software
             for clients ranging from individuals and small-businesses all the
             way to large enterprise corporations. What would you do if you had
